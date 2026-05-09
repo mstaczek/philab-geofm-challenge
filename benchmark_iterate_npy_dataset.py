2f289159d5ad97b592eaf73b4c337311eefa88bb
mstaczek <matemmatem2000@gmail.com>
2026-05-10 00:18:24 +0200

    Add short benchmark of original tif dataset, got x7.2 speed-up

--- benchmark_iterate_npy_dataset.py
+++ benchmark_iterate_npy_dataset.py
@@ -1,6 +1,23 @@
 import time
 
-from multi_folder_dataset import MultiFolderNpyDataset
+from multi_folder_dataset import MultiFolderDataset, MultiFolderNpyDataset
+
+    
+def benchmark_dataset(dataset, name, limit=2048):
+    print(f"\nBenchmarking: {name}")
+    start = time.perf_counter()
+    count = 0
+    for sample in dataset:
+        count += 1
+        if count >= limit:
+            break
+
+    counted = min(len(dataset), count)
+
+    elapsed = time.perf_counter() - start
+    print(f"Samples: {counted}")
+    print(f"Time: {elapsed:.3f} sec")
+    print(f"Samples/sec: {counted / elapsed:.2f}")
 
 
 train_dataset = MultiFolderNpyDataset(
@@ -29,27 +46,6 @@ test_dataset = MultiFolderNpyDataset(
     ],
 )
 
-sample = train_dataset[0]
-
-for k, v in sample.items():
-    print(k, v.shape, v.dtype)
-    
-def benchmark_dataset(dataset, name, limit=2048):
-    print(f"\nBenchmarking: {name}")
-    start = time.perf_counter()
-    count = 0
-    for sample in dataset:
-        count += 1
-        if count >= limit:
-            break
-
-    counted = min(len(dataset), count)
-
-    elapsed = time.perf_counter() - start
-    print(f"Samples: {counted}")
-    print(f"Time: {elapsed:.3f} sec")
-    print(f"Samples/sec: {counted / elapsed:.2f}")
-
 benchmark_dataset(train_dataset, "train npy")
 benchmark_dataset(test_dataset, "test npy")
 
@@ -62,3 +58,45 @@ benchmark_dataset(test_dataset, "test npy")
 # Samples: 946
 # Time: 128.030 sec
 # Samples/sec: 7.39
+
+
+
+train_dataset_non_npy = MultiFolderDataset(
+    root="data/embed2heights/data",
+    split="train",
+    input_folders=[
+        "alphaearth_emb",
+        "terramind_s1_emb",
+        "terramind_s2_emb",
+        "tessera_emb",
+        "thor_s1_emb",
+        "thor_s2_emb",
+    ],
+)
+
+test_dataset_non_npy = MultiFolderDataset(
+    root="data/embed2heights/data",
+    split="test",
+    input_folders=[
+        "alphaearth_test_emb",
+        "terramind_test_s1_emb",
+        "terramind_test_s2_emb",
+        "tessera_test_emb",
+        "thor_test_s1_emb",
+        "thor_test_s2_emb",
+    ],
+)
+
+
+benchmark_dataset(train_dataset_non_npy, "train tif", limit=128)
+benchmark_dataset(test_dataset_non_npy, "test tif", limit=128)
+
+# Benchmarking: train tif
+# Samples: 128
+# Time: 96.060 sec
+# Samples/sec: 1.33
+
+# Benchmarking: test tif
+# Samples: 128
+# Time: 126.120 sec
+# Samples/sec: 1.01
